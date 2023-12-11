d6cf2ccf8ad760b5bfbe51425a7d9b1c6d329716
Kelly Santos <kelly_cris19@outlook.com.br>
2023-12-10 23:08:04 -0300

    Conclusão do desafio POO da DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia: ");
+Nokia nokia = new Nokia(numero: "(87) 91325-2741", modelo: "Modelo 1", imei: "985785948982629", memoria:64) ;
+nokia.Ligar();
+nokia.InstalarAplicativo("Moovit");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Iphone iphone= new Iphone(numero: "(61) 91947-2230", modelo: "Modelo 2", imei: "984933144855691", memoria:128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Duolingo");
+
